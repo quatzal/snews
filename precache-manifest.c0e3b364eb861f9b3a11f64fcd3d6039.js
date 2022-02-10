901a6446af6f19b84bc0333868a8057a2305f635
quatzal <quatzal451@gmail.com>
2022-02-10 23:19:13 +0100

    Updates

---
+++ precache-manifest.c0e3b364eb861f9b3a11f64fcd3d6039.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "803a35f280d17ec275accdd1485d3d3b",
+    "url": "/snews/asset-manifest.json"
+  },
+  {
+    "revision": "c007c20ce2cec338ac038f1b0ca9a8b0",
+    "url": "/snews/index.html"
+  },
+  {
+    "revision": "6e950d30a11f5ab4d190",
+    "url": "/snews/static/css/main.87b8e2e3.css"
+  },
+  {
+    "revision": "6f3c9ddc52841350b7eb",
+    "url": "/snews/static/js/787.827c5c6d.chunk.js"
+  },
+  {
+    "revision": "6e950d30a11f5ab4d190",
+    "url": "/snews/static/js/main.f8db4c7b.js"
+  },
+  {
+    "revision": "8543a91b75d2510224bc5f25cfbec005",
+    "url": "/snews/static/js/main.f8db4c7b.js.LICENSE.txt"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/snews/precache-manifest.f157f3c354a930d1267217cfdc211029.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/snews/precache-manifest.c0e3b364eb861f9b3a11f64fcd3d6039.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 /* eslint-disable no-undef */
 if (workbox) {
